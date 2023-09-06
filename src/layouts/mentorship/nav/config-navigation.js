6c75af4808dfaeb193129279a24d950c8b68f80f
Luca Carrozzo <carrozzo.lc@gmail.com>
2023-09-06 15:50:59 +0200

    added pages

---
+++ src/layouts/mentorship/nav/config-navigation.js
@@ -0,0 +1,15 @@
+
+// ----------------------------------------------------------------------
+
+const NAV_ITEMS = [
+  {
+    label: 'Mentorship for career',
+    href: '/career',
+  },
+  {
+    label: 'Mentorship for entrepeneur',
+    href: '/entrepeneur',
+  },
+];
+
+export default NAV_ITEMS;
