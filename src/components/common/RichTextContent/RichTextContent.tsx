1e9b1c824d230b3df5f2d884cdc512fe59523dde
Luca Carrozzo <carrozzo.lc@gmail.com>
2024-06-19 17:44:01 +0200

    content update and new page

--- src/components/common/RichTextContent/RichTextContent.tsx
+++ src/components/common/RichTextContent/RichTextContent.tsx
@@ -8,6 +8,17 @@ const RichTextContent = ({ content }: { content: string | TrustedHTML }) => {
       <Text 
         as="div" fontSize={'md'}
         sx={{
+          'h2': {
+            fontSize: '24px',
+            fontWeight: '700',
+            pb: '12px',
+            pt: '8px'
+          },
+          'h3': {
+            fontSize: '21px',
+            fontWeight: '700',
+            mb: '10px'       
+          },
           'p': {
             mb: 4
           },
